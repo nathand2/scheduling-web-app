ea57da3126657d25b5e3b1e13d85e537ddf37643
Nathan <ndong5@my.bcit.ca>
2022-06-10 23:31:38 -0700

    setting up database table

--- backend/init.sql
+++ backend/init.sql
@@ -12,6 +12,46 @@ CREATE TABLE users (
     PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS sessions;
+CREATE TABLE sessions (
+  id int NOT NULL AUTO_INCREMENT UNIQUE,
+  group_id int NOT NULL,
+  session_title varchar(50) NOT NULL,
+  session_desc varchar (255),
+  max_session_members int NOT NULL DEFAULT 10,
+  FOREIGN KEY (group_id) REFERENCES group(id)
+);
+
+DROP TABLE IF EXISTS group;
+CREATE TABLE group (
+  id int NOT NULL AUTO_INCREMENT UNIQUE,
+  group_name varchar (50) NOTNULL,
+  dt_created datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  max_group_members int NOT NULL DEFAULT 20,
+);
+
+DROP TABLE IF EXISTS group_invitation;
+CREATE TABLE group_invitation (
+  id bigint NOT NULL AUTO_INCREMENT UNIQUE,
+  group_id int NOT NULL,
+  dt_created datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  dt_expires datetime DEFAULT DATEADD(hour,1,CURRENT_TIMESTAMP) NOT NULL,
+  max_group_members int NOT NULL DEFAULT 20,
+  PRIMARY KEY (id),
+  FOREIGN KEY (group_id) REFERENCES group(id)
+);
+
+DROP TABLE IF EXISTS user_group;
+CREATE TABLE user_group (
+  id bigint NOT NULL AUTO_INCREMENT UNIQUE,
+  group_id int NOT NULL,
+  user_id int NOT NULL,
+  max_group_members int NOT NULL DEFAULT 20,
+  PRIMARY KEY (id),
+  FOREIGN KEY (group_id) REFERENCES group(id),
+  FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
 USE scheduler; SELECT * FROM users WHERE google_id = "112150195852534406291";
 
 INSERT INTO users (username, password, nickname) VALUES ("Nathan", "potatoman", "Nate");
