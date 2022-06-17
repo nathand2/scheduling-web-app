0ed772da459bb46c0ff8fee30990624e675ae3ac
Nathan <ndong5@my.bcit.ca>
2022-06-17 16:57:51 -0700

    auth user context

--- backend/authServer.js
+++ backend/authServer.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const cors = require('cors')
+const crypto = require('crypto')
 require('dotenv').config() // Environment variables stored in .env file
 
 const auth = require('./services/jwtAuth');
@@ -9,9 +10,59 @@ const db = require('./db');
 const port = 6500;
 const app = express();
 
+// For Cookie security
+const secureCookieConfig = {
+  secure: true,
+  httpOnly: true,
+  sameSite: 'strict' // Won't work if api and auth on different domains. Helps against CSRF attacks.
+}
+
+
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 app.use(express.json());
+
+//Middleware
+app.use(express.json());
+app.use(passport.initialize());
+require('./services/googleStrategy');
+
+
+/**
+ * Gets random string.
+ * @returns random string
+ */
+const getRandomString = async () => {
+  return await new Promise((resolve, reject) => {
+    crypto.randomBytes(48, (err, buffer) => {
+      if (err) {
+        reject(-1);
+      }
+      resolve(buffer.toString('base64'));
+    });
+  });
+}
+
+/**
+ * Hashes a string using crypto. TODO: Consider using bcrypt
+ * @param {string} input 
+ * @returns hashed string
+ */
+const hashString = (input) => {
+  const hash = crypto.createHash('sha256').update(input).digest('base64');
+  return hash
+}
+
+
+app.get('/test', async (req, res) => {
+  // const randomString = await getRandomString();
+  // console.log("Random String:", randomString)
+  // const hash = hashString(randomString);
+  // console.log("Hashed String:", hash)
+  res.json({stuff: "potato"})
+})
 
 /**
  * Route to refresh access token using refresh token
@@ -38,30 +89,46 @@ app.post('/token', async (req, res, next) => {
 }, auth.refreshAccessToken // Callback
 )
 
-//Middleware
-app.use(express.json());
-app.use(passport.initialize());
-require('./services/googleStrategy');
-
 app.get('/auth/google',
   passport.authenticate('google', { scope: [ 'email', 'profile' ]})
 );
 
 app.get('/auth/google/callback', passport.authenticate( 'google', {
-  // failureRedirect: 'http://localhost:5500/login',
+  failureRedirect: 'http://localhost:3000/login',
   failWithError: true,
   session: false
-}), (req, res, next) => {
+}), async (req, res, next) => {
+
+  // Generate random string and hash for user context verification.
+  const randomString = await getRandomString();
+  console.log("Random String:", randomString)
+  const hash = hashString(randomString);
+  console.log("Hashed String:", hash)
 
   // On successful authentication, respond with JWT token.
-  const user = {name: req.user.id}
+  const user = {
+    name: req.user.id,
+    hash: hash
+  }
+
   const accessToken = auth.generateAccessToken(user);
   const refreshToken = auth.generateRefreshToken(user);
-  
+  console.log("accessToken:", accessToken)
+  console.log("refreshToken:", refreshToken)
+
   // Add token to db
   try {
     db.insertRefreshToken(refreshToken);
-    res.json({ accessToken: accessToken, refreshToken: refreshToken });
+
+    // Unsecure cookies for tokens to be stored in session storage.
+    res.cookie('accessToken', accessToken)
+    res.cookie('refreshToken', refreshToken)
+
+    // Secure, hardened cookies
+    res.cookie('userContext', randomString, secureCookieConfig);
+
+    res.redirect('http://localhost:3000/')
+
   } catch(err) {
     res.sendStatus(500); // Internal Error (database error)
   }
