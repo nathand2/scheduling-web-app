1b1bbf14977eb88c5284dfb42fd89bc067eb1f57
Nathan <ndong5@my.bcit.ca>
2022-06-29 17:30:11 -0700

    clean up

--- frontend/src/components/Sessions.js
+++ frontend/src/components/Sessions.js
@@ -7,61 +7,17 @@ import SessionCard from './SessionCard'
 
 import {RequestHandler} from '../js/requestHandler'
 
-const fakeSessions = [
-  {
-    id: 0,
-    title: "Session Title",
-    desc: "Session description",
-    dt_created: "Monday? feb 5th?",
-    status: "upcoming",
-  },
-  {
-    id: 1,
-    title: "Session Title",
-    desc: "Session description",
-    dt_created: "Monday? feb 5th?",
-    status: "upcoming",
-  },
-  {
-    id: 2,
-    title: "Session Title",
-    desc: "Session description",
-    dt_created: "Monday? feb 5th?",
-    status: "upcoming",
-  },
-  {
-    id: 3,
-    title: "Session Title",
-    desc: "Session description",
-    dt_created: "Monday? feb 5th?",
-    status: "expired",
-  },
-  {
-    id: 4,
-    title: "Session Title",
-    desc: "Session description",
-    dt_created: "Monday? feb 5th?",
-    status: "expired",
-  },
-]
-
-
 const Sessions = () => {
-  
   const [sessions, setSessions] = useState([])
   
   useEffect(() => {
     // Get sessions
-    // const userSessions = fakeSessions;
     let didCancel = false;
     const getSessions = async () => {
       if (!didCancel) {
         // Get session data from api
-        console.log("Hello world")
-        // const params = useParams();
         try {
           const data = await RequestHandler.req(`/sessions`, 'GET')
-          // return data
           console.log("Res data:", data.sessions)
           setSessions(data.sessions)
         } catch(err) {
@@ -76,7 +32,6 @@ const Sessions = () => {
     <div>
       Sessions
       <Container className='sessions-preview d-flex flex-wrap bd-highlight'>
-      {/* <a href='/sessioncreate'> */}
         <Card style={{ width: '18rem' }} bg="secondary" text="white">
             <Link to='/sessioncreate' className='link-plain'>
             <Card.Body>
@@ -87,7 +42,6 @@ const Sessions = () => {
             </Card.Body>
           </Link>
         </Card>
-        {/* </a> */}
         {
           sessions.map((session) => (<SessionCard key={session.id} code={session.code} title={session.session_title} desc={session.session_desc} dt_created={session.dt_created} status={"Status"}  />))
         }
