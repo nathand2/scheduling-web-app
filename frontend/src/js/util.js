ae3dc6794d48cb3f8abaf1e4edf1e04cc7f6f085
Nathan <ndong5@my.bcit.ca>
2022-07-06 13:23:03 -0700

    clean up

--- frontend/src/js/util.js
+++ frontend/src/js/util.js
@@ -1,25 +1,6 @@
-// exports.mySqlDtToJsDate = (dtString) => {
-//   let dateTimeParts= dtString.split(/[- :]/); // regular expression split that creates array with: year, month, day, hour, minutes, seconds values
-//   dateTimeParts[1]--; // monthIndex begins with 0 for January and ends with 11 for December so we need to decrement by one
-
-//   const dateObject = new Date(...dateTimeParts); // our Date object
-//   return dateObject
-// }
-
-// exports.mySqlDtToJsDate = (dtString) => {
-//   var t = dtString.split(/[- :]/);
-
-//   // Apply each element to the Date function
-//   var d = new Date(Date.UTC(t[0], t[1]-1, t[2], t[3], t[4], t[5]));
-
-//   console.log(d);
-//   return d
-// }
-
 exports.mySqlDtToJsDate = (dtString) => {
   var t = dtString.split(/[- :T.]/);
   console.log("Split date?:", t)
   const date = new Date(Date.UTC(t[0], t[1] - 1, t[2], t[3], t[4], t[5]))
-  // console.log(date)
   return date
 }
