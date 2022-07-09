89065b8519d4694bbac1fd93c9b4493b8788b4cb
Nathan <ndong5@my.bcit.ca>
2022-07-08 21:56:44 -0700

    session chart work

--- frontend/src/components/SessionChart.js
+++ frontend/src/components/SessionChart.js
@@ -1,16 +1,15 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef } from "react";
 import Button from "react-bootstrap/Button";
-import * as d3 from 'd3'
+import * as d3 from "d3";
 const util = require("../js/util");
 
 const SessionChart = ({ timeRanges, session }) => {
-
-  const canvas = useRef(null)
+  const canvas = useRef(null);
 
   const chartScale = 5;
 
   // const [onEffectOnce, setOnEffectOnce] = useState(true)
-  let stop = false
+  let stop = false;
 
   // useEffect(() => {
   //   if (!stop) {
@@ -20,70 +19,114 @@ const SessionChart = ({ timeRanges, session }) => {
   //   }
   // }, [timeRanges])
 
-  const generateChart = () => {
-    const ranges = processTimeRanges()
-    drawBarChart(ranges)
+  const generateDateTicks = () => {
+    let ticks = []
+    // timeRanges.map((range) => {
+
+    // })
+    let currentDate;
+    for (let i = 0; i < timeRanges.length; i++) {
+      if (!currentDate) {
+        ticks.push(currentDatetoLocaleDateString(undefined, options))
+      }
+    }
   }
+
+  const generateChart = () => {
+    const ranges = processTimeRanges();
+    drawBarChart(ranges);
+  };
 
   const drawBarChart = (data) => {
-    const sessionLengthInMinutes = (session.dt_end - session.dt_start) / (1000 * 60) * chartScale // Session length in minutes
-    console.log("sessionLengthInMinutes", sessionLengthInMinutes)
-    const canvasHeight = sessionLengthInMinutes
-    const canvasWidth = 500
-    const barWidth = 80
-    const barGap = 5
-    const barColor = 'lightblue'
-    const svgCanvas = d3.select(canvas.current)
-      .append('svg')
+    const sessionLengthInMinutes =
+      ((session.dt_end - session.dt_start) / (1000 * 60)) * chartScale; // Session length in minutes
+    console.log("sessionLengthInMinutes", sessionLengthInMinutes);
+    const canvasMargin = { top: 20, right: 30, bottom: 30, left: 40 };
+    const canvasHeight =
+      sessionLengthInMinutes + canvasMargin.top + canvasMargin.bottom;
+    const canvasWidth = 500;
+    const barWidth = 80;
+    const barGap = 5;
+    const barColor = "lightblue";
+    const svgCanvas = d3
+      .select(canvas.current)
+      .append("svg")
       .attr("class", "bar")
-      .attr('width', canvasWidth)
-      .attr('height', canvasHeight)
-      .style('border', '1px solid black')
-    svgCanvas.selectAll('rect')
-      .data(data).enter()
-      .append('rect')
-      .attr('width', barWidth)
-      .attr('height', (datapoint) => datapoint.height)
-      .attr('fill', barColor)
-      .attr('x', (datapoint, iteration) => iteration * (barWidth + barGap))
-      .attr('y', (datapoint) => datapoint.top)
+      .attr("width", canvasWidth)
+      .attr("height", canvasHeight)
+      .style("border", "1px solid black");
 
-    // svgCanvas.selectAll('text')
-    // .data(data).enter()
-    //     .append('text')
-    //     .attr('x', (dataPoint, i) => i * 45 + 10)
-    //     .attr('y', (dataPoint, i) => canvasHeight - dataPoint * scale - 10)
-    //     .text(dataPoint => dataPoint)
-  }
+    // Y-Axis Start
+    // Create scale
+    var yScale = d3
+      .scaleLinear()
+      .domain([0, sessionLengthInMinutes])
+      .range([canvasMargin.top, canvasHeight - canvasMargin.bottom]); // This works kinda
+
+    // Add scales to axis
+    var yAxis = d3.axisLeft().scale(yScale).tickSize(-canvasWidth); // Lines across ticks
+    
+    svgCanvas
+      .append("g")
+      .attr("transform", `translate(${canvasMargin.left}, 0)`)
+      //  .attr("transform", "translate(" + "0" + ",0)")
+      .call(yAxis)
+    
+    svgCanvas.selectAll(".tick line")
+      .attr("stroke","steelBlue");
+    // Y-Axis End
+
+    // Draw rects
+    svgCanvas
+      .selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("width", barWidth)
+      .attr("height", (datapoint) => datapoint.height)
+      .attr("fill", barColor)
+      .attr(
+        "x",
+        (datapoint, iteration) =>
+          iteration * (barWidth + barGap) + canvasMargin.left
+      )
+      .attr("y", (datapoint) => datapoint.top);
+
+    
+  };
 
   const processTimeRanges = () => {
-    console.log("Session:", session)
-    let ranges = []
+    console.log("Session:", session);
+    let ranges = [];
     timeRanges.map((range) => {
-      const dtRangeStart = util.mySqlDtToJsDate(range.dt_start)
-      const dtRangeEnd = util.mySqlDtToJsDate(range.dt_end)
+      const dtRangeStart = util.mySqlDtToJsDate(range.dt_start);
+      const dtRangeEnd = util.mySqlDtToJsDate(range.dt_end);
 
       // Height of the bar
-      const minuteRangeDifference = (dtRangeEnd - dtRangeStart) / (1000 * 60)  // Get minute diff
-      
-      // Space from top of chart to top of bar
-      const minuteStartDifference = (dtRangeStart - session.dt_start) / (1000 * 60)  // Get minute diff
-      console.log("minuteRangeDifference:", minuteRangeDifference)
-      console.log("minuteStartDifference:", minuteStartDifference)
+      const minuteRangeDifference = (dtRangeEnd - dtRangeStart) / (1000 * 60); // Get minute diff
 
-      ranges.push({height: Math.trunc(minuteRangeDifference) * chartScale, top: Math.trunc(minuteStartDifference) * chartScale})
-    })
-    console.log("Range stuff:", ranges)
+      // Space from top of chart to top of bar
+      const minuteStartDifference =
+        (dtRangeStart - session.dt_start) / (1000 * 60); // Get minute diff
+      console.log("minuteRangeDifference:", minuteRangeDifference);
+      console.log("minuteStartDifference:", minuteStartDifference);
+
+      ranges.push({
+        height: Math.trunc(minuteRangeDifference) * chartScale,
+        top: Math.trunc(minuteStartDifference) * chartScale,
+      });
+    });
+    console.log("Range stuff:", ranges);
     // setData(ranges)
-    return ranges
-  }
+    return ranges;
+  };
 
   return (
     <div>
-      <Button onClick={generateChart} >Draw chart</Button>
+      <Button onClick={generateChart}>Draw chart</Button>
       <div className="canvas-container" ref={canvas}></div>
     </div>
-  )
-}
+  );
+};
 
-export default SessionChart
+export default SessionChart;
